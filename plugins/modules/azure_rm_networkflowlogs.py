1578d83856ee5c700dfc236fdb4ef7b4dc5e47e9
Fred-sun <xiuxi.sun@qq.com>
2024-05-24 22:55:43 +0800

    Support to manage network watcher relate modules

---
+++ plugins/modules/azure_rm_networkflowlogs.py
@@ -0,0 +1,283 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2024 xuzhang3 (@xuzhang3), Fred-sun (@Fred-sun)
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+DOCUMENTATION = '''
+---
+module: azure_rm_networkflowlogs
+version_added: "2.5.0"
+short_description: Manage the network watcher
+description:
+    - Create, update or delete the network watcher.
+options:
+    resource_group:
+        description:
+            - Name of resource group.
+        required: true
+        type: str
+    location:
+        description:
+            - Valid Azure location. Defaults to location of the resource group.
+        type: str
+    name:
+        description:
+            - The name of the SSH public key.
+        required: true
+        type: str
+    state:
+        description:
+            - State of the Flow Logs. Use C(present) to create or update and C(absent) to delete.
+        default: present
+        type: str
+        choices:
+            - absent
+            - present
+
+extends_documentation_fragment:
+    - azure.azcollection.azure
+    - azure.azcollection.azure_tags
+
+author:
+    - xuzhang3 (@xuzhang3)
+    - Fred-sun (@Fred-sun)
+
+'''
+
+EXAMPLES = '''
+- name: Create a Flow Logs
+  azure_rm_networkflowlogs:
+    resource_group: myResourceGroup
+    name: myNetworkWatcher
+    location: eastus
+    tags:
+      testing: testing
+      delete: on-exit
+
+- name: Delete a Flow Logs
+  azure_rm_networkflowlogs:
+    resource_group: myResourceGroup
+    name: myNetworkWatcher
+    state: absent
+'''
+RETURN = '''
+state:
+    description:
+        - The facts of the network watcher.
+    returned: always
+    type: complex
+    contains:
+        resource_group:
+            description:
+                - The resource group.
+            type: str
+            returned: always
+            sample: NetworkWatcherRG
+        id:
+            description:
+                - Resource ID.
+            returned: always
+            type: str
+            sample: "/subscriptions/xxx-xxx/resourceGroups/NetworkWatcherRG/providers/Microsoft.Network/networkWatchers/netwatcher_eastus"
+        location:
+            description:
+                - Resource location.
+            returned: always
+            type: str
+            sample: eastus
+        name:
+            description:
+                - Resource name.
+            returned: always
+            type: str
+            sample: mynetworkwatcher01
+        tags:
+            description:
+                - Resource tags.
+            returned: always
+            type: dict
+            sample: { 'key1':'value1' }
+        type:
+            description:
+                - Resource type.
+            returned: always
+            type: str
+            sample: "Microsoft.Network/networkWatchers"
+        provisioning_state:
+            description:
+                - The provisioning state of the network watcher resource.
+            type: str
+            returned: always
+            sample: Succeeded
+'''
+
+try:
+    from azure.core.exceptions import ResourceNotFoundError
+except ImportError:
+    # This is handled in azure_rm_common
+    pass
+
+from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common import AzureRMModuleBase
+
+
+class AzureRMNetworkWatcher(AzureRMModuleBase):
+
+    def __init__(self):
+
+        self.module_arg_spec = dict(
+            resource_group=dict(type='str', required=True),
+            network_watcher=dict(type='str', required=True),
+            name=dict(type='str', required=True),
+            state=dict(type='str', default='present', choices=['present', 'absent']),
+            location=dict(type='str'),
+            target_resource_id=dict(type='str'),
+            storage_id=dict(type='str'),
+            enabled=dict(type='bool'),
+            retention_policy=dict(type='dict',
+                options=dict(
+                    days=dict(type='int'),
+                    enabled=dict(type='bool'),
+                ),
+            ),
+            flow_analytics_configuration=dict(
+                type='dict',
+                opitions=dict(
+                    network_watcher_flow_analytics_configuration=dict(
+                        type='dict'
+                        options=dict(
+                            enabled=dict(type='bool'),
+                            workspace_id=dict(type='str'),
+                            workspace_region=dict(type='str'),
+                            workspace_resource_id=dict(type='str'),
+                            traffic_analytics_interval=dict(type='int')
+                    )
+                )
+            ),
+        )
+
+        self.resource_group = None
+        self.name = None
+        self.state = None
+        self.location = None
+        self.tags = None
+
+        self.results = dict(
+            changed=False,
+            state=dict()
+        )
+
+        super(AzureRMNetworkWatcher, self).__init__(self.module_arg_spec,
+                                                  supports_tags=True,
+                                                  supports_check_mode=True)
+
+    def exec_module(self, **kwargs):
+
+        for key in list(self.module_arg_spec.keys()) + ['tags']:
+            setattr(self, key, kwargs[key])
+
+        resource_group = self.get_resource_group(self.resource_group)
+        if not self.location:
+            # Set default location
+            self.location = resource_group.location
+
+        changed = False
+        results = dict()
+
+        old_response = self.get_by_name()
+
+        if old_response is not None:
+            if self.state == 'present':
+                update_tags, new_tags = self.update_tags(old_response['tags'])
+                if update_tags:
+                    changed = True
+                    if not self.check_mode:
+                        results = self.update_tags(dict(tags=new_tags))
+                else:
+                    results = old_response
+            else:
+                changed = True
+                if not self.check_mode:
+                    results = self.delete_network_watcher()
+        else:
+            if self.state == 'present':
+                changed = True
+                if not self.check_mode:
+                    results = self.create_or_update(dict(tags=self.tags, location=self.location))
+            else:
+                changed = False
+                self.log("The Flow Logs is not exists")
+
+        self.results['changed'] = changed
+        self.results['state'] = results
+
+        return self.results
+
+    def get_by_name(self):
+        response = None
+        try:
+            response = self.network_client.flow_logs.get(self.resource_group, self.network_watcher_name, self.name)
+
+        except ResourceNotFoundError as exec:
+            self.log("Failed to get network flow logs, Exception as {0}".format(exec))
+
+        return self.to_dict(response)
+
+    def create_or_update(self, body):
+        response = None
+        parameter = dict(
+                tags={'key1': 'value1', 'key2': 'value2'},
+                storage_id= ""
+            )
+        try:
+            response = self.to_dict(self.network_client.flow_logs.begin_create_or_update(self.resource_group, self.network_watcher_name, self.name, body))
+        except Exception as exc:
+            self.fail("Error creating Flow Logs {0} - {1}".format(self.name, str(exc)))
+
+        return response
+
+    def update_tags(self, body):
+        response = None
+        try:
+            response = self.network_client.flow_logs.update_tags(self.resource_group, self.network_watcher_name, self.name, body)
+        except Exception as exc:
+            self.fail("Error updating Flow Logs {0} - {1}".format(self.name, str(exc)))
+        return self.to_dict(response)
+
+    def delete_flow_logs(self):
+        try:
+            self.network_client.flow_logs.begin_delete(self.resource_group, self.network_watcher_name, self.name)
+        except Exception as exc:
+            self.fail("Error deleting Flow Logs {0} - {1}".format(self.name, str(exc)))
+
+    def to_dict(self, body):
+        results = None
+        if body is not None:
+            results = dict(
+                resource_group=self.resource_group,
+                network_watcher_name=self.network_watcher_name,
+                id=body.id,
+                name=body.name,
+                location=body.location,
+                tags=body.tags,
+                type=body.type,
+                provisioning_state=body.provisioning_state,
+                target_resource_id=body.target_resource_id,
+                storage_id=body.storage_id,
+                enabled=body.enabled,
+                retention_policy=body.retention_policy,
+                flow_analytics_configuration=body.flow_analytics_configuration,
+            )
+        return results
+
+
+def main():
+    AzureRMNetworkWatcher()
+
+
+if __name__ == '__main__':
+    main()
---
+++ plugins/modules/azure_rm_networkflowlogs_info.py
@@ -0,0 +1,210 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2024 xuzhang3 (@xuzhang3), Fred-sun (@Fred-sun)
+#
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+
+DOCUMENTATION = '''
+---
+module: azure_rm_networkflowlogs_info
+
+version_added: "2.5.0"
+
+short_description: Get or list the network flow logs
+
+description:
+    - Get or list the network flow logs facts.
+
+options:
+    resource_group:
+        description:
+            - The name of the resource group.
+        type: str
+        required: true
+    network_wather_name:
+        description:
+            - Name of the network watcher.
+        type: str
+        required: true
+    name:
+        description:
+            - Name of the network flow logs.
+        type: str
+    tags:
+        description:
+            - Limit results by providing a list of tags. Format tags as 'key' or 'key:value'.
+        type: list
+        elements: str
+
+extends_documentation_fragment:
+    - azure.azcollection.azure
+
+author:
+    - xuzhang3 (@xuzhang3)
+    - Fred-sun (@Fred-sun)
+
+'''
+
+EXAMPLES = '''
+- name: Get the network watcher facts
+  azure_rm_networkflowlogs_info:
+    resource_group: myResourceGroup
+    network_watcher_name: mywatcher01
+    name: flowlogname
+
+- name: list the network flow logs and filter by tags
+  azure_rm_networkflowlogs_info:
+    resource_group: myResourceGroup
+    network_watcher_name: mywatcher01
+'''
+
+RETURN = '''
+flow_logs:
+    description:
+        - The facts of the network flow logs.
+    returned: always
+    type: complex
+    contains:
+        resource_group:
+            description:
+                - The resource group.
+            type: str
+            returned: always
+            sample: NetworkWatcherRG
+        id:
+            description:
+                - Resource ID.
+            returned: always
+            type: str
+            sample: "/subscriptions/xxx-xxx/resourceGroups/NetworkWatcherRG/providers/Microsoft.Network/networkWatchers/netwatcher_eastus"
+        location:
+            description:
+                - Resource location.
+            returned: always
+            type: str
+            sample: eastus
+        name:
+            description:
+                - Resource name.
+            returned: always
+            type: str
+            sample: mynetworkwatcher01
+        tags:
+            description:
+                - Resource tags.
+            returned: always
+            type: dict
+            sample: { 'key1':'value1' }
+        type:
+            description:
+                - Resource type.
+            returned: always
+            type: str
+            sample: "Microsoft.Network/networkWatchers"
+        provisioning_state:
+            description:
+                - The provisioning state of the network watcher resource.
+            type: str
+            returned: always
+            sample: Succeeded
+'''
+
+try:
+    from azure.core.exceptions import ResourceNotFoundError
+    import logging
+    logging.basicConfig(filename='log.log', level=logging.INFO)
+except Exception:
+    # This is handled in azure_rm_common
+    pass
+
+from ansible_collections.azure.azcollection.plugins.module_utils.azure_rm_common import AzureRMModuleBase
+
+
+class AzureRMNetworkFlowLogsInfo(AzureRMModuleBase):
+
+    def __init__(self):
+
+        self.module_arg_spec = dict(
+            resource_group=dict(type='str', required=True),
+            network_watcher_name=dict(type='str', required=True),
+            name=dict(type='str'),
+            tags=dict(type='list', elements='str')
+        )
+
+        self.results = dict(
+            changed=False,
+            flow_logs=[]
+        )
+
+        self.resource_group = None
+        self.network_watcher_name = None
+        self.name = None
+        self.tags = None
+
+        super(AzureRMNetworkFlowLogsInfo, self).__init__(self.module_arg_spec,
+                                                      supports_check_mode=True,
+                                                      supports_tags=False,
+                                                      facts_module=True)
+
+    def exec_module(self, **kwargs):
+        for key in self.module_arg_spec:
+            setattr(self, key, kwargs[key])
+
+        if self.name:
+            response = [self.get_by_name()]
+        else:
+            response = self.list_by_network_watcher()
+
+        self.results['flow_logs'] = [self.to_dict(item) for item in response if response is not None]
+
+        return self.results
+
+    def get_by_name(self):
+        response = None
+        try:
+            response = self.network_client.flow_logs.get(self.resource_group, self.network_watcher, self.name)
+
+        except ResourceNotFoundError as exec:
+            self.log("Failed to get network flow logs, Exception as {0}".format(exec))
+
+        return response
+
+    def list_by_network_watcher(self):
+        response = []
+        try:
+            response = self.network_client.flow_logs.list(self.resource_group)
+        except Exception as exec:
+            self.log("Faild to list network flow logs by network watcher, exception as {0}".format(exec))
+        return response
+
+    def to_dict(self, body):
+        results = dict()
+        if body is not None and self.has_tags(body.tags, self.tags):
+            results = dict(
+                resource_group=self.resource_group,
+                network_watcher_name=self.network_watcher_name,
+                id=body.id,
+                name=body.name,
+                location=body.location,
+                tags=body.tags,
+                type=body.type,
+                provisioning_state=body.provisioning_state,
+                target_resource_id=body.target_resource_id,
+                storage_id=body.storage_id,
+                enabled=body.enabled,
+                retention_policy=body.retention_policy,
+                flow_analytics_configuration=body.flow_analytics_configuration,
+            )
+        return results
+
+
+def main():
+    AzureRMNetworkFlowLogsInfo()
+
+
+if __name__ == '__main__':
+    main()
