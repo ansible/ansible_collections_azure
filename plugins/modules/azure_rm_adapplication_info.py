081786ff3790459811064c725613a0f14b95fbd9
Fred-sun <xiuxi.sun@qq.com>
2024-06-14 11:00:05 +0800

    new change

--- plugins/modules/azure_rm_adapplication_info.py
+++ plugins/modules/azure_rm_adapplication_info.py
@@ -72,8 +72,8 @@ EXAMPLES = '''
 - name: get ad app diff ---- by display name
   azure_rm_adapplication_info:
     app_diff:
-        - app_display_name: "{{ display_name }}"
-        - app_id: "{{ app_id }}"
+      - app_display_name: "{{ display_name }}"
+      - app_id: "{{ app_id }}"
 '''
 
 RETURN = '''
