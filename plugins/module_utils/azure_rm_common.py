5232dbfa2e091be54fb383ec7d452ac51f8f9ae1
Fred-sun <xiuxi.sun@qq.com>
2024-10-25 17:31:48 +0800

    small change

--- plugins/module_utils/azure_rm_common.py
+++ plugins/module_utils/azure_rm_common.py
@@ -429,7 +429,7 @@ class AzureRMModuleBase(object):
         self._resource_client = None
         self._compute_client = None
         self._disk_client = None
-        self.multi_disk_client = None
+        self._multi_disk_client = None
         self._diskencryptionset_client = None
         self._image_client = None
         self._dns_client = None
