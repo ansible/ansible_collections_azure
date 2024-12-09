9237acc0fadc082c6b9005ee789abe1f195a56ff
Ross Bender <l3ender@users.noreply.github.com>
2024-12-08 19:51:58 -0600

    Update plugins/modules/azure_rm_appgateway.py
    Co-authored-by: Fred-sun <37327967+Fred-sun@users.noreply.github.com>

--- plugins/modules/azure_rm_appgateway.py
+++ plugins/modules/azure_rm_appgateway.py
@@ -2649,7 +2649,8 @@ class AzureRMApplicationGateways(AzureRMModuleBaseExt):
             del waf_config['firewall_policy']['force_association']
             self.parameters['firewall_policy'] = waf_config['firewall_policy']
         else:
-            self.module.deprecate("WAF configuration can no longer be added to an application gateway."
+            self.module.deprecate("WAF configuration can no longer be added to an application gateway.
+                                  Please use a WAF policy via 'firewall_policy' instead.", version="4.0.0")
                                   "Please use a WAF policy via 'firewall_policy' instead.", version=(4.0.0 ))
 
 
